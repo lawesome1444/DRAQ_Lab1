427f20f566674572866321878d8fff8dc375869a
Conor <pacman1444@gmail.com>
2023-09-29 10:02:38 +0100

    ex2 part 4 finished

--- ex2pt4.js
+++ ex2pt4.js
@@ -2,7 +2,12 @@
 const ages = [25, 31, 42, 77];
 
 let partD = ages.map((item)=>{
-    return item * 2;
+    //If the number is greater or equal to 70, do not double it. Otherwise, double the value
+    if (item >= 70)
+        return item;
+    else
+        return item * 2;
 })
 
+//Call partD function and print the array out to console
 console.log(partD);
