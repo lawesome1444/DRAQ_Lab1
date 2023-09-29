54604c60a307e5e9885a8e632bd5db9865c865b2
Conor <pacman1444@gmail.com>
2023-09-29 09:44:20 +0100

    Ex2 part 3 finished

--- ex2pt3.js
+++ ex2pt3.js
@@ -1,4 +1,8 @@
 //2(c) Arrow function returning the sum of 2 numbers
 let sumOfDataRep = (a, b)=>{
-    
+    //Get the sum of the numbers
+    sumOfDataRep = a + b;
+    //Print to console
+    console.log(sumOfDataRep);
 }
+sumOfDataRep(5, 4);
