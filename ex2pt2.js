80534602cbfa1cbe4cafc1a219c874cf0868dddc
Conor <pacman1444@gmail.com>
2023-09-29 10:41:38 +0100

    (b) Improved comments and function names

--- ex2pt2.js
+++ ex2pt2.js
@@ -1,5 +1,8 @@
 //2(b) Arrow function accepting variables
-let varDataRep = (argument)=>{
+//When called, this function takes the passed value, assigns it to "argument" and then prints argument
+//Done to make the function more compact
+let passedVarArrowFunc = (argument)=>{
     console.log(argument);
 }
-varDataRep("This will be passed to the varDataRep Arrow function!");
+//Pass the long string of text to the arrow function above so it can be printed to console
+passedVarArrowFunc("This will be passed to the varDataRep Arrow function!");
